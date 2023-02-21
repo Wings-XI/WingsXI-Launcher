2289a0ec37a2849ec6fe486c9f85ea6da70317ab
lbegnaud <lbegnaud@radersolutions.com>
2023-02-21 04:59:20 -0600

    update ibar plugin from Tokey

--- patch_input/Ashita/addons/ibar/ibar.lua
+++ patch_input/Ashita/addons/ibar/ibar.lua
@@ -22,9 +22,9 @@
  *	DEALINGS IN THE SOFTWARE.
 ]]--
 
-_addon.author   = 'Vicrelant';
+_addon.author   = 'Tokey (modified original works by Vicrelant)';
 _addon.name     = 'ibar';
-_addon.version  = '3.0.2';
+_addon.version  = '3.0.6.7';
 
 require 'common'
 
@@ -63,22 +63,32 @@ local default_config =
 {
     font =
     {
-        name        = 'Arial',
+        name        = 'Calibri',
         size        = 10,
         color		= '255,255,255,255',
         position    = { 130, 0 },
-        bgcolor     = '200,0,0,0',
+        bgcolor     = '100,0,0,0',
         bgvisible   = true,
-		bold		= true
+		bold		= false
     },
 	layout =
 	{
-		player = '$zone $name  [$level]  [$position] - $ecompass',
-		target = '$target  [$job / $level / $aggro] [id: $id] Weak[$weak]  [$position]',
-		npc = '$target [$position] [ID: $id / Index: $m_index]'
+		player = ' $zone ($z_id) $name  [$level]  [$position] - $ecompass ',
+		targetNew = ' $target [$job / $level / $aggro] [ID: $id] $position  $weak  $strong  $respawn  $steal  $items ',
+		npc = ' $target [$position] [ID: $id / Index: $m_index] '
+	},
+	lootpools =
+	{
+		drops = true,
+		steal = true
+	},
+	options =
+	{
+		targetPosition = false
 	}
 };
 local ibar_config = default_config;
+local f = AshitaCore:GetFontManager():Create( '__ibar_wings_addon' );
 
 ---------------------------------------------------------------------------------------------------
 -- func: load
@@ -92,7 +102,6 @@ ashita.register_event('load', function()
 	local a,r,g,b = ibar_config.font.bgcolor:match("([^,]+),([^,]+),([^,]+),([^,]+)");
 	local bcolor = math.d3dcolor(a,r,g,b);
 	
-	local f = AshitaCore:GetFontManager():Create( '__ibar_addon' );
     f:SetBold( ibar_config.font.bold );
     f:SetColor( fcolor );
     f:SetFontFamily( ibar_config.font.name );
@@ -119,12 +128,12 @@ end );
 -- desc: Called when our addon is unloaded.
 ---------------------------------------------------------------------------------------------------
 ashita.register_event('unload', function()
-	local f = AshitaCore:GetFontManager():Get( '__ibar_addon' );
+	local f = AshitaCore:GetFontManager():Get( '__ibar_wings_addon' );
 	ibar_config.font.position = { f:GetPositionX(), f:GetPositionY() };
 	
 	ashita.settings.save(_addon.path .. 'settings/ibar.json', ibar_config);
 	
-	AshitaCore:GetFontManager():Delete( '__ibar_addon' );
+	AshitaCore:GetFontManager():Delete( '__ibar_wings_addon' );
 end );
 
 ---------------------------------------------------------------------------------------------------
@@ -132,7 +141,7 @@ end );
 -- desc: Called when our addon is rendered.
 ---------------------------------------------------------------------------------------------------
 ashita.register_event('render', function()
-    local f         = AshitaCore:GetFontManager():Get( '__ibar_addon' );
+    local f         = AshitaCore:GetFontManager():Get( '__ibar_wings_addon' );
 	local Entity	= AshitaCore:GetDataManager():GetEntity();
     local party     = AshitaCore:GetDataManager():GetParty();
 	local player	= AshitaCore:GetDataManager():GetPlayer();
@@ -274,7 +283,7 @@ ashita.register_event('render', function()
 		end
 	end
 	
-	local m_target = ibar_config.layout.target;
+	local m_target = ibar_config.layout.targetNew;
 	
 		  name = string.find(m_target,'$target');
 		  zone = string.find(m_target,'$zone');
@@ -285,74 +294,109 @@ ashita.register_event('render', function()
 	local flag = string.find(m_target,'$aggro');
 	local mjob = string.find(m_target,'$job');
 	local weak = string.find(m_target,'$weak');
+	local strong = string.find(m_target,'$strong');
 	local m_hp = string.find(m_target,'$hpp');
+	local respawn = string.find(m_target, '$respawn');
+	local items = string.find(m_target, '$items');
+	local steal = string.find(m_target, '$steal');
+
 	
+
+
+
 	-- attempt to obtain target information.
 	if (target:GetTargetServerId() ~= nil) then
 		for i = 1, arraySize do
 			if (tonumber(mb_data[i].id) == target:GetTargetServerId()) then
-				if (mb_data[i].sj == mb_data[i].mj) then
-					
-					local tentity = GetEntity(target:GetTargetIndex());
-					pX = string.format('%2.3f',tentity.Movement.LocalPosition.X);
-					pY = string.format('%2.3f',tentity.Movement.LocalPosition.Y);
-					pZ = string.format('%2.3f',tentity.Movement.LocalPosition.Z);
-					
-					if (name ~= nil) then m_target = string.gsub(m_target,'$target',tentity.Name); end
-					if (zone ~= nil) then m_target = string.gsub(m_target,'$zone',ZoneName); end
-					if (m_id ~= nil) then m_target = string.gsub(m_target,'$id',target:GetTargetServerId()); end
-					if (m_ix ~= nil) then m_target = string.gsub(m_target,'$m_index',target:GetTargetIndex()); end
-					if (mjob ~= nil) then m_target = string.gsub(m_target,'$job',jobs[tonumber(mb_data[i].mj)]); end
-					if (mlvl ~= nil) then m_target = string.gsub(m_target,'$level',mb_data[i].mlvl); end
-					if (gpos ~= nil) then m_target = string.gsub(m_target,'$position', pX .. ',' .. pY .. ',' .. pZ); end
-					if (weak ~= nil) then m_target = string.gsub(m_target,'$weak',mb_data[i].weak); end
-					if (m_hp ~= nil) then m_target = string.gsub(m_target,'$hpp',tentity.HealthPercent); end
-					
-					if (flag ~= nil) then
-						if (mb_data[i].links == 'Y') then
-							m_target = string.gsub(m_target,'$aggro',mb_data[i].aggro .. ',L');
-						else
-							m_target = string.gsub(m_target,'$aggro',mb_data[i].aggro);
-						end
-					end
-					
-					f:SetText(string.format(m_target));
-					return;
-				
+
+				local tentity = GetEntity(target:GetTargetIndex());
+				pX = string.format('%2.3f',tentity.Movement.LocalPosition.X);
+				pY = string.format('%2.3f',tentity.Movement.LocalPosition.Y);
+				pZ = string.format('%2.3f',tentity.Movement.LocalPosition.Z);
+
+				if (name ~= nil) then m_target = string.gsub(m_target,'$target',tentity.Name); end
+				if (zone ~= nil) then m_target = string.gsub(m_target,'$zone',ZoneName); end
+				if (m_id ~= nil) then m_target = string.gsub(m_target,'$id',target:GetTargetServerId()); end
+				if (m_ix ~= nil) then m_target = string.gsub(m_target,'$m_index',target:GetTargetIndex()); end
+
+				if (mlvl ~= nil) then m_target = string.gsub(m_target,'$level',mb_data[i].mlvl); end
+
+				if (mb_data[i].weak ~= "") then
+					if (weak ~= nil) then m_target = string.gsub(m_target,'$weak','[Weak: ' .. mb_data[i].weak ..']'); end
 				else
-				
-					local tentity = GetEntity(target:GetTargetIndex());
-					pX = string.format('%2.3f',tentity.Movement.LocalPosition.X);
-					pY = string.format('%2.3f',tentity.Movement.LocalPosition.Y);
-					pZ = string.format('%2.3f',tentity.Movement.LocalPosition.Z);
-					
-					if (name ~= nil) then m_target = string.gsub(m_target,'$target',tentity.Name); end
-					if (zone ~= nil) then m_target = string.gsub(m_target,'$zone',ZoneName); end
-					if (m_id ~= nil) then m_target = string.gsub(m_target,'$id',target:GetTargetServerId()); end
-					if (m_ix ~= nil) then m_target = string.gsub(m_target,'$m_index',target:GetTargetIndex()); end
-					if (mlvl ~= nil) then m_target = string.gsub(m_target,'$level',mb_data[i].mlvl); end
-					if (gpos ~= nil) then m_target = string.gsub(m_target,'$position', pX .. ',' .. pY .. ',' .. pZ); end
-					if (weak ~= nil) then m_target = string.gsub(m_target,'$weak',mb_data[i].weak); end
-					if (m_hp ~= nil) then m_target = string.gsub(m_target,'$hpp',tentity.HealthPercent);  end
-					
+					if (weak ~= nil) then m_target = string.gsub(m_target,'$weak','[Weak: N/A]'); end
+				end
+
+				if (mb_data[i].strong ~= "") then
+					if (strong ~= nil) then m_target = string.gsub(m_target,'$strong', '[Strong: ' .. mb_data[i].strong .. ']'); end
+				else
+					if (strong ~= nil) then m_target = string.gsub(m_target,'$strong', '[Strong: N/A]'); end
+				end
+
+				if (m_hp ~= nil) then m_target = string.gsub(m_target,'$hpp',tentity.HealthPercent); end
+
+				if (mb_data[i].sj == mb_data[i].mj) then
+					if (mjob ~= nil) then m_target = string.gsub(m_target,'$job',jobs[tonumber(mb_data[i].mj)]); end
+				else
 					if (mjob ~= nil) then
 						m_target = string.gsub(m_target,'$job',
 						jobs[tonumber(mb_data[i].mj)] .. '/' ..
 						jobs[tonumber(mb_data[i].sj)]);
 					end
-					
-					if (flag ~= nil) then
-						if (mb_data[i].links == 'Y') then
-							m_target = string.gsub(m_target,'$aggro',mb_data[i].aggro .. ',L');
-						else
-							m_target = string.gsub(m_target,'$aggro',mb_data[i].aggro);
-						end
-					end
-					
-					f:SetText(string.format(m_target));
-					return;
-				
 				end
+
+				if (ibar_config.options.targetPosition) then
+					if (gpos ~= nil) then m_target = string.gsub(m_target,'$position', '[' .. pX .. ',' .. pY .. ',' .. pZ ..']'); end
+				else
+					m_target = string.gsub(m_target,' $position ', '')
+				end
+
+
+				if (respawn ~= nil) then
+					if (tonumber(mb_data[i].respawntime) > 0) then
+						if (tonumber(mb_data[i].respawntime) < 60) then
+							m_target = string.gsub(m_target,'$respawn','[Respawn: ' .. mb_data[i].respawntime .. 'sec]');
+						elseif (tonumber(mb_data[i].respawntime) > 7200) then
+							local n = (mb_data[i].respawntime / 3600)
+							n = tonumber(string.format("%." .. (1 or 0) .. "f", n))
+
+								m_target = string.gsub(m_target,'$respawn', '[Respawn: ' .. n .. 'hr]');
+						else
+							m_target = string.gsub(m_target,'$respawn','[Respawn: ' .. string.format('%.2f', (mb_data[i].respawntime / 60)) .. 'min]');
+						end
+					else
+						m_target = string.gsub(m_target,'$respawn','[Respawn: N/A]');
+					end
+				end
+
+				-- remove extra space before adding in the new line character
+				-- m_target = m_target:gsub("%s+", " ");
+
+				if (ibar_config.lootpools.drops == true and items ~= nil and mb_data[i].items ~= "") then
+					m_target = string.gsub(m_target, '$items', '[Drops: ' .. mb_data[i].items .. ']')
+				else
+					m_target = string.gsub(m_target, '$items', "[Drops: N/A]")
+				end
+
+				if (ibar_config.lootpools.steal == true and steal ~= nil and mb_data[i].steal ~= "") then
+					m_target = string.gsub(m_target, '$steal', '[Steal: ' .. mb_data[i].steal .. ']')
+				else
+					m_target = string.gsub(m_target, '$steal', "[Steal: N/A]")
+				end
+
+				if (flag ~= nil) then
+					if (mb_data[i].links == 'Y') then
+						m_target = string.gsub(m_target,'$aggro',mb_data[i].aggro .. ',L');
+					else
+						m_target = string.gsub(m_target,'$aggro',mb_data[i].aggro);
+					end
+				end
+				
+				-- m_target = m_target:gsub("[^%S\n]+", " ");
+				f:SetText(string.format(m_target));
+
+				return;
+
 			end
 		end
 		
@@ -364,7 +408,12 @@ ashita.register_event('render', function()
 		n_hp = string.find(m_target,'$hpp');
 		
 		local tentity = GetEntity(target:GetTargetIndex());
+
+
 		if (tentity ~= nil) then
+
+			-- for k, v in pairs(getmetatable(tentity)) do			print(k .. ': ' .. type(v))			end
+
 			pX = string.format('%2.3f',tentity.Movement.LocalPosition.X);
 			pY = string.format('%2.3f',tentity.Movement.LocalPosition.Y);
 			pZ = string.format('%2.3f',tentity.Movement.LocalPosition.Z);
@@ -413,3 +462,44 @@ ashita.register_event('incoming_packet', function(id, size, packet)
 	
     return false;
 end );
+
+---------------------------------------------------------------------------------------------------
+-- func: command
+-- desc: Event called when a command was entered.
+---------------------------------------------------------------------------------------------------
+ashita.register_event('command', function(command, ntype)
+    -- Get the command arguments..
+    local args = command:args();
+    if (args[1] ~= '/ibar') then
+        return false;
+    end
+
+	if (args[2] == nil or args[2] == "help") then
+		print('  \30\02 /ibar pos x y\30\71 - Manually set the position of the bar. Note: you can still use shift+click to drag');
+		print('  \30\02 /ibar fontsize number\30\71 - Adjust the font size of the ibar. Only allows values between 8 and 20');
+	end
+
+	if (args[2] == "fontsize") then
+		local fontsize = tonumber(args[3])
+		if (fontsize < 8 or fontsize > 20) then
+			return false;
+		end
+		local f = AshitaCore:GetFontManager():Get( '__ibar_wings_addon' );
+		ibar_config.font.size = fontsize
+		f:SetFontHeight( ibar_config.font.size );
+		ashita.settings.save(_addon.path .. 'settings/ibar.json', ibar_config);
+	end
+
+	if (args[2] == "pos") then
+		local f = AshitaCore:GetFontManager():Get( '__ibar_wings_addon' );
+		local x = tonumber(args[3])
+		local y = tonumber(args[4])
+		print(string.format('move ibar to %d %d', x, y))
+		ibar_config.font.position = {x, y}
+		f:SetPositionX( ibar_config.font.position[1] );
+		f:SetPositionY( ibar_config.font.position[2] );
+		ashita.settings.save(_addon.path .. 'settings/ibar.json', ibar_config);
+	end
+
+	return true;
+end);
