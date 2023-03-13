1276905bdab930a2aec016f457d735561678102d
lbegnaud <lbegnaud@radersolutions.com>
2023-03-13 12:08:54 -0500

    group weak/strong info

--- patch_input/Ashita/addons/ibar/data/67.lua
+++ patch_input/Ashita/addons/ibar/data/67.lua
@@ -4,5 +4,5 @@
  *	Total	::	1
 ]]--
 	mb_data = {}
-	mb_data[1] = { nm="N", id="17051694", name="Lancelord_Gaheel_Ja", mj="7", sj="7", mlvl="75-75", behavior="0", aggro="A,T(S)", links="N", spawntype="128", respawntime="0", items="", steal="", weak="Ice: 1.3, Lightning: 1.15, Dark: 1.15", strong="Wind: 1.3", note="" }
+	mb_data[1] = { nm="N", id="17051694", name="Lancelord_Gaheel_Ja", mj="7", sj="7", mlvl="75-75", behavior="0", aggro="A,T(S)", links="N", spawntype="128", respawntime="0", items="", steal="", weak="Ice: 1.3, Lightning/Dark: 1.15", strong="Wind: 1.3", note="" }
 	return mb_data;
