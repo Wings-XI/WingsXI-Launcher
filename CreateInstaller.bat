2b40122ff320bcc1f64a0101a760f17d5d8a17d8
MowFord <131182600+MowFord@users.noreply.github.com>
2023-06-20 17:22:06 -0500

    exclude dll files as well

--- CreateInstaller.bat
+++ CreateInstaller.bat
@@ -1,5 +1,6 @@
 cd %~dp0
 robocopy /S "includes" "build"
-robocopy /S "build" "patch_input" /XF patch.exe patch.dat version.txt *.sft *.mfx *.ift
+del /q patch_input\data
+robocopy /S "build" "patch_input" /XF patch.exe patch.dat version.txt *.sft *.mfx *.ift *.dll
 robocopy /S "build" "patch_input" /IF launchcfg
 start "" Install.iit
